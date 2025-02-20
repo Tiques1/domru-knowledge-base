14f7e74c649fefad8f9165c5ff94058448d74cb2
Tiques1 <tiques13@mail.ru>
2025-02-20 20:06:42 +0500

    add charset to fix encoding problem

--- web-server.ps1
+++ web-server.ps1
@@ -1,26 +1,19 @@
-# first open powershell
-# second execute command: Set-ExecutionPolicy -Scope CurrentUser unrestricted
-# write y
-# start the script
-Set-ExecutionPolicy -Scope CurrentUser unrestricted
-
 $httpListener = New-Object System.Net.HttpListener
 $httpListener.Prefixes.Add("http://localhost:9090/")
 $httpListener.Start()
 # New-NetFirewallRule -DisplayName "AllowTestWebServer" -Direction Inbound -Protocol TCP -LocalPort 9090 -Action Allow
 
-# write-host "Press any key to stop the HTTP listener after next request"
-while (!([console]::KeyAvailable)) { # !([console]::KeyAvailable)
+while (!([console]::KeyAvailable)) {
 $context = $httpListener.GetContext()
 
 $urlReq = $context.Request.Url -split ("/")
 $urlReqs = ".\" + $urlReq[-1]
 $EncodingWebContent = ""
 $context.Response.ContentType = ""
-# write-host $urlReq[-1]
 
 if ($urlReq[-1] -eq "") {
-    $WebContent = Get-Content -Path ".\main.html" -Encoding UTF8
+    $WebContent = Get-Content -Path ".\main.html"
+    $context.Response.ContentType = "text/html; charset=utf-8"
 }
 else {
     $WebContent = Get-Content -Path $urlReqs -Encoding UTF8
@@ -28,16 +21,14 @@ else {
     
 
     if ($fileType[-1] -eq "css") {
-        $context.Response.ContentType = "text/css"
+        $context.Response.ContentType = "text/css; charset=utf-8"
     }
     if ($fileType[-1] -eq "js") {
-        $context.Response.ContentType = "text/javascript"
+        $context.Response.ContentType = "text/javascript; charset=utf-8"
     }
 }
 
 $context.Response.StatusCode = 200
-# $context.Response.ContentType = 'text/HTML'
-# $WebContent = Get-Content -Path ".\main.html" -Encoding UTF8
 $EncodingWebContent = [Text.Encoding]::UTF8.GetBytes($WebContent)
 $context.Response.OutputStream.Write($EncodingWebContent , 0, $EncodingWebContent.Length)
 $context.Response.Close()
